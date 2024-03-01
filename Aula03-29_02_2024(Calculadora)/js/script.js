343ce980e2b97874db5e8cebbf1fa9fea5642afb
paulo a.c <pauloaugustocosta9@gmail.com>
2024-02-29 21:42:03 -0300

    background; col-6; css

--- Aula03-29_02_2024(Calculadora)/js/script.js
+++ Aula03-29_02_2024(Calculadora)/js/script.js
@@ -1,29 +1,31 @@
+
 var num1 = document.getElementById('num1')
 var num2 = document.getElementById('num2')
 var resultado = document.getElementById('resultado')
 
 function somar(){
     var soma = parseFloat(num1.value) + parseFloat(num2.value)
-    resultado.innerHTML = (soma)
+    resultado.innerHTML = ('O resultado da soma é: ' + soma)
 }
 
 function subtrair(){
     var subtracao = parseFloat(num1.value) - parseFloat(num2.value)
-    resultado.innerHTML = (subtracao)
+    resultado.innerHTML = ('O resultado da subtração é: ' + subtracao)
 }
 
 function dividir(){
     var divisao = parseFloat(num1.value) / parseFloat(num2.value)
-    resultado.innerHTML = (divisao)
+    resultado.innerHTML = ('O resultado da divisão é: ' + divisao)
 }
 
 function multiplicar(){
     var multiplicacao = parseFloat(num1.value) * parseFloat(num2.value)
-    resultado.innerHTML = (multiplicacao)
+    resultado.innerHTML = ('O resultado da multiplicação é: ' + multiplicacao)
 }
 
 function limpar(){
     num1.value = ''
     num2.value = ''
-    resultado.innerHTML = ''
+    resultado.innerHTML = 'Você ainda não fez nenhum cálculo.'
 }
+
