9781c390ba4e451e76519421cae2fdaf29521579
paulo a.c <pauloaugustocosta9@gmail.com>
2024-04-04 18:29:32 -0300

    sombraimg; centralização; fonte

--- Aula06-28_03_2024(CSS;Flashy)/modoclaroescuro/js/script.js
+++ Aula06-28_03_2024(CSS;Flashy)/modoclaroescuro/js/script.js
@@ -5,7 +5,7 @@ titulo.textContent = 'Aula 09 Manipular CSS'
 titulo.innerHTML = 'Manipular CSS'
 let imagem = document.querySelector('#foto')
 imagem.setAttribute('src', 'img/nuvens.jpeg')
-imagem.setAttribute('width', '280px')
+imagem.setAttribute('width', '260vw')
 
 /* MANIPULAR CSS */
 document.querySelector('h1').style.color = "white";
@@ -14,6 +14,9 @@ titulo.style.backgroundColor = "#000";
 titulo.style.borderBottom = "4px solid blue";
 titulo.style.padding = "0.625rem";
 titulo.style.borderRadius = "15px";
+
+let nomeimagem = document.querySelector('h2')
+let txtimg = document.querySelector('#textoimg')
 
 let box = document.querySelectorAll('.box')
 box[0].setAttribute('class', 'escura')
@@ -34,6 +37,9 @@ btnBlue.addEventListener('click', modoBlue)
 btnYellow.addEventListener('click', modoYellow)
 btnPink.addEventListener('click', modoPink)
 
+imagem.style.borderRadius = "15px";
+imagem.style.boxShadow = "0px 0px 10px rgba(0, 0, 0, 0.5)";
+
 function modoDark() {
     event.preventDefault();
     console.log('modo dark')
@@ -44,6 +50,8 @@ function modoDark() {
     tela.classList.remove("rosa");
     imagem.setAttribute('src', 'img/esqueleto.jpeg');
     titulo.style.borderBottom = "4px solid white";
+    nomeimagem.innerHTML = "Esqueleto";
+    txtimg.innerHTML = "Imagem de um esqueleto";
 }
 
 function modoLight() {
@@ -56,6 +64,8 @@ function modoLight() {
     tela.classList.remove("rosa");
     imagem.setAttribute('src', 'img/nuvens.jpeg');
     titulo.style.borderBottom = "4px solid blue";
+    nomeimagem.innerHTML = "Nuvens";
+    txtimg.innerHTML = "Imagem de nuvens";
 }
 
 function modoBlue() {
@@ -68,6 +78,8 @@ function modoBlue() {
     tela.classList.remove("rosa");
     imagem.setAttribute('src', 'img/mar.jpeg');
     titulo.style.borderBottom = "4px solid white";
+    nomeimagem.innerHTML = "Mar";
+    txtimg.innerHTML = "Imagem do mar";
 }
 
 function modoYellow() {
@@ -80,6 +92,8 @@ function modoYellow() {
     tela.classList.remove("rosa");
     imagem.setAttribute('src', 'img/abelha.jpg');
     titulo.style.borderBottom = "4px solid blue";
+    nomeimagem.innerHTML = "Abelha";
+    txtimg.innerHTML = "Imagem de uma abelha";
 }
 
 function modoPink() {
@@ -92,6 +106,8 @@ function modoPink() {
     tela.classList.add("rosa");
     imagem.setAttribute('src', 'img/ipe.jpg');
     titulo.style.borderBottom = "4px solid blue";
+    nomeimagem.innerHTML = "Ipê";
+    txtimg.innerHTML = "Imagem de um ipê";
 }
 
 /*
