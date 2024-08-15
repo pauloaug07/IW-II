71e375f682bed44bcf99b419e49350006614f99e
pauloaug07 <paulo.costa181@etec.sp.gov.br>
2024-08-15 09:08:49 -0300

    index.css (correção margin); container e cards

--- 3°Bimestre/react/etec-filmes/src/App.js
+++ 3°Bimestre/react/etec-filmes/src/App.js
@@ -1,15 +1,21 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Banner from "./components/Banner";
-// import Container from "./components/Container";
+import Container from "./components/Container";
+import Card from "./components/Card";
+import videos from "./json/db.json"
 
 function App() {
   return (
     <>
       <Header/>
-      <Banner/>
-      <h1>ETEC MCM</h1>
-      <p>Teste de react</p>
+      <Banner image="home"/>
+      <Container>
+        <h2>Geografia</h2>
+        <section className="cards">
+          { videos.map((video) => <Card id={video.id} key={video.id}/> )}
+        </section>
+      </Container>
       <Footer/>
     </>
   );
