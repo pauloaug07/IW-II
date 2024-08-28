f4797c2a431874553e418a65df3fa646eec0421e
paulo a.c <pauloaugustocosta9@gmail.com>
2024-08-28 18:33:14 -0300

    componente Category; filterCategory; solução de mapeamento, export, import e escopo.

--- 3°Bimestre/react/etec-filmes/src/App.js
+++ 3°Bimestre/react/etec-filmes/src/App.js
@@ -3,22 +3,27 @@ import Footer from "./components/Footer";
 import Banner from "./components/Banner";
 import Container from "./components/Container";
 import Card from "./components/Card";
-import videos from "./json/db.json"
+import Category, { categories, filterCategory } from "./components/Category";
 
 function App() {
+
   return (
     <>
-      <Header/>
-      <Banner image="home"/>
+      <Header />
+      <Banner image="home" />
       <Container>
-        <h2>Geografia</h2>
-        <section className="cards">
-          { videos.map((video) => <Card id={video.id} key={video.id}/> )}
-        </section>
+
+        { categories.map((category, index) =>
+          <Category category={category}>
+            {filterCategory(index).map((video) => <Card id={video.id} key={video.id} />)}
+          </Category>
+        )}
+
       </Container>
-      <Footer/>
+      <Footer />
     </>
   );
+
 }
 
 export default App;
